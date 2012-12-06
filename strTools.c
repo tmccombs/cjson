4a674d0abe805d71a12bfd6f4d12c3f638dde42e
tmccombs <astrothayne@gmail.com>
2012-12-05 22:47:38 -0700

    changed strTools to increase strings by a constant buffer size rather than doubling the size

--- strTools.c
+++ strTools.c
@@ -4,6 +4,7 @@
 
 #include "strTools.h"
 
+#define SCAT_BUFFER_SIZE     512         // size to increase buffer by when we run out of length
 
 //safely concatenat src onto dest, reallocating more memory if necessary.
 //length is the number of chars currently in dest
@@ -12,7 +13,7 @@
 char *safe_cat(char **dest, size_t *length, char *src){
     size_t dest_l = strlen(*dest), src_l = strlen(src);
     if( dest_l + src_l + 1 > *length) {
-        *length *= 2;
+        *length += SCAT_BUFFER_SIZE;
         *dest = realloc(*dest,*length*sizeof(char));
     }
     return strcat(*dest,src);
